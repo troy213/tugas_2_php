82a872a2b89a362169c41b20ea38addf1409bfb6
troy213 <49593922+troy213@users.noreply.github.com>
2021-01-18 00:20:33 +0700

    Add files via upload

---
+++ tugas2_1.php
@@ -0,0 +1,11 @@
+<?php
+  $a = 36;
+  $b = 12;
+  $y = 24;
+  $x = $b / $y * $a;
+
+  echo "$a Hari = $b Pekerja <br>";
+  echo "X hari = $y Pekerja  <br>";
+  echo "X = ...?  <br>";
+  echo "Jawaban : $x Hari";
+?>
---
+++ tugas2_2.php
@@ -0,0 +1,10 @@
+<?php
+  $jual = 80000;
+  $beli = 50000;
+  $untung = $jual - $beli;
+
+  echo "Jual = $jual <br>";
+  echo "Beli = $beli <br>";
+  echo "Keuntungan = ?  <br>";
+  echo "Jawaban : $untung";
+?>
